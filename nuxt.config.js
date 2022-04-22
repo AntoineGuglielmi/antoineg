b0e6389659ab672a056661295cf6101149a5e90f
Antoine Guglielmi <antoine.guglielmi@insign.fr>
2022-04-22 14:08:35 +0200

    POST request cors error

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,7 +61,8 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     // baseURL: '/',
-    proxy: true
+    proxy: true,
+    credentials: false
   },
 
   proxy: {
