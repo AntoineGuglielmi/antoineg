580da33eceb4c9ad25a6d2024798e9453af421b1
Antoine Guglielmi <antoine.guglielmi@insign.fr>
2022-04-22 14:26:26 +0200

    aze

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,6 +62,7 @@ export default {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     // baseURL: '/',
     proxy: true,
+    proxyHeaders: false,
     credentials: false
   },
 
